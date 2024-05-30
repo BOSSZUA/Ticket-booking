958cbd4bf41bedb9d7e489b033047f11bfc8ae15
Bosszua <ifebos7@gmail.com>
2024-05-30 10:23:05 +0100

    ticket booking website

---
+++ script.js
@@ -0,0 +1,40 @@
+
+document.getElementById("ticket-form").addEventListener("submit",function(event){
+    event.preventDefault();
+
+    //Gather user input
+    var name = document.getElementById("name").value;
+    var seat = document.getElementById("seat").value;
+    var date = document.getElementById("date").value;
+    var from = document.getElementById("from").value;
+    var to = document.getElementById("to").value;
+
+
+    //Generate ticket ID(simple demonstration)
+    var ticketId = "T" + Math.floor(Math.random() * 10000);
+
+    //Add tickect details to history table
+    var tableBody = document.getElementById("history-table-body");
+    var newRow = tableBody.insertRow();
+    newRow.innerHTML = "<td>" + ticketId + "</td><td>" + name + "</td><td>" + seat + "</td><td>" + date + "</td><td>" + from + "</td><td>" + to + "</td><td>booked</td><td><button class= 'delete-btn' data-ticket-id='" + ticketId + "'>Delete></button></td>";
+
+    //Display alert for sucessful booking and generated ticket ID
+    alert ("Ticket with ID"  + ticketId + " has booked successfully.");
+    document.getElementById("ticket-form").reset();
+
+});
+
+document.getElementById("view-history-btn").addEventListener("click", function(){
+    document.getElementById("ticket-history").style.display = "block";
+});
+
+document.getElementById("tickect-history").addEventListener("click", function(event){
+    if(event.target.classlist.contains('delete-btn')) {
+        var ticketIdToDelete = event.target.getAttribute('data-ticket-id');
+
+        //delete tickect logic(for demonstration and will remove row from the table)
+        event.target.closest('tr').remove();
+        alert("Ticket with ID" + ticketIdToDelete + " has been deleted.");
+    }
+    
+});
